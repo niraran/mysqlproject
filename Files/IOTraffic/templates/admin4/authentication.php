8a7738952bec020a7dac56f35bda76721f309d00
nir aran <niraran@gmail.com>
2015-07-31 14:17:31 +0300

    auth works !

--- Files/IOTraffic/templates/admin4/authentication.php
+++ Files/IOTraffic/templates/admin4/authentication.php
@@ -1,46 +1,32 @@
+<?php include 'views\DataBase.php';?>
 <?php
 //set vars
-$user = $_POST['username'];
-$pass = sha1($_POST['password']);
-//Open Connection to mySQL
-$db_host = "localhost" ;
-$db_username = "root" ;
-$db_pass = "" ;
-$db_name = "iotraffic" ;
+extract($_POST);
+$username = $_POST['username'];
+$password = sha1($_POST['password']);
+//$pass = sha1($password);
+$i=0;
 
+$queryuser = "SELECT user_name FROM users WHERE user_name = '$username'";
+$result = mysql_query($queryuser);
+$temp_user = mysql_result($result,$i);
+if ($temp_user == $username){
+  $querypass = "SELECT password FROM users WHERE password = '$password'";
+  $resultpass = mysql_query($querypass);
+  $temp_pass = mysql_result($resultpass,$i);
+  //echo mysql_result($temp_pass,$i);
+  if($temp_pass == $password){
+    echo "grate";
+    header("location:index.php");
+  }
+  else {
+    header("location:login.php");
 
-if ($user&&$pass)
-{
-//connect to db
-$connection = mysql_connect($db_host,$db_username,$db_pass);
-//$connect = mysql_connect("$server","$username","$password") or die("not connecting");
-mysql_select_db($db_name) or die("no db :'(");
-$queryuser = mysql_query("SELECT * FROM users WHERE user_name=='$user'");
-$querypass = mysql_query("SELECT * FROM users WHERE password=='$pass'");
-
-
-if  ($user == $queryuser && $pass == $querypass){
-	header("location:traffic_index.html");
   }
 
+
 }
-else
-  echo "user does not exist!";
+else {
+header("location:login.php");
 }
-else
-    die("please enter a username and password!");
-
-
-
-//echo print_r($_POST);
-
-/* if (!isset($_SERVER['PHP_AUTH_USER'])) {
-    header('WWW-Authenticate: Basic realm="My Realm"');
-    header('HTTP/1.0 401 Unauthorized');
-    echo 'Text to send if user hits Cancel button';
-    exit;
-} else {
-    echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
-    echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
-} */
 ?>
