fcd8cb2ec35018fa3ebcfa6ccf7853bc3796c514
nir aran <niraran@gmail.com>
2015-07-31 11:21:29 +0300

    fix db query

--- Files/IOTraffic/templates/admin4/test.php
+++ Files/IOTraffic/templates/admin4/test.php
@@ -5,6 +5,7 @@ $username='niraran';
 $pass = '123';
 //echo [$user];
 $i=0;
+$j=0;
 //if ($user&&$pass)
 //{
 
@@ -14,16 +15,15 @@ $queryuser = "SELECT user_name FROM users WHERE user_name = '$username'";
 $result = mysql_query($queryuser);
 $temp_user = mysql_result($result,$i);
 if ($temp_user == $username){
-  $querypass = "SELECT * FROM users WHERE password = '$pass'";
+  $querypass = "SELECT password FROM users WHERE password = '$pass'";
   $resultpass = mysql_query($querypass);
   $temp_pass = mysql_result($resultpass,$i);
-  echo mysql_result($temp_pass,$i);
-
+  //echo mysql_result($temp_pass,$i);
   if($temp_pass == $pass){
     echo "grate";
   }
   else {
-    echo mysql_result($temp_pass,$i);
+    //echo mysql_result($temp_pass,$j);
     echo "not";
   }
   // //SELECT * FROM `users` WHERE `user_name` = "niraran" && `password`
